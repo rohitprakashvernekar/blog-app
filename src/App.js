607db5b200386f1bacc39ae8ad3229b7564b93f2
rohitvernekar7 <rohitvernekar7@yahoo.com>
2021-01-31 14:07:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 
 function App() {
+  const title = 'Welcome to the new blog';
+  const likes = 50;
   return (
     <div className="App">
       <div className="content">
-        <h1>App component</h1>
+        <h1>{title}</h1>
+        <p> Liked {likes} times</p>
+
       </div>
     </div>
-  );
+  ); 
 }
 
 export default App;
