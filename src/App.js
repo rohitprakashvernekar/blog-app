034ba3ad281cfdf00b0fc3fb4a5ff56f22399122
rohitvernekar7 <rohitvernekar7@yahoo.com>
2021-01-31 14:47:29 +0530

    added navbar and homepage component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import './App.css';
+import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
-  const title = 'Welcome to the new blog';
-  const likes = 50;
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{title}</h1>
-        <p> Liked {likes} times</p>
-
+      <Home />
       </div>
     </div>
-  ); 
+  );
 }
 
 export default App;
