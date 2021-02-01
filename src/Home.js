9e955578b528190d86883fe89b2820ccc6f2986f
rohitvernekar7 <rohitvernekar7@yahoo.com>
2021-02-01 21:27:43 +0530

    added css and blog-preview

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,37 @@
-import React from 'react'
+import { useState } from "react";
 
 const Home = () => {
-    return (
-        <div className="home">
-        <h2>Homepage</h2>
-            
-        </div>
-    )
-}
+  const [blogs, setBlogs] = useState([
+    {
+      title: "My New Website",
+      body: "loreem ipsum...",
+      author: "Rohit",
+      id: 1,
+    },
+    {
+      title: "Welcome",
+      body: "loreem ipsum...",
+      author: "Vernekar",
+      id: 2,
+    },
+    {
+      title: "Web Dev",
+      body: "loreem ipsum...",
+      author: "RPV",
+      id: 3,
+    },
+  ]);
 
-export default Home
+  return (
+    <div className="home">
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Home;
