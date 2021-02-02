1057aa03add0cea18c53b5f402d40b21e93608c5
rohitvernekar7 <rohitvernekar7@yahoo.com>
2021-02-02 21:28:23 +0530

    added useFetch custom hook,Added route, createpage,db,json file

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,17 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
-
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "My New Website", body: "loreem ipsum...",author: "Rohit",id: 1,},
-    { title: "Welcome",body: "loreem ipsum...",author: "Vernekar",id: 2,},
-    { title: "Web Dev",body: "loreem ipsum...",author: "RPV",id: 3,},
-  ]);
-
-  const handleDelete =(id) =>{
-    const newBlogs = blogs.filter(blog =>blog.id!==id);
-    setBlogs(newBlogs)
-  }
+  const { data: blogs, isPending, error } = useFetch(
+    "http://localhost:8000/blogs"
+  );
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
     </div>
   );
 };
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,13 +1,11 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <nav className="navbar">
       <h1> Rohit blog</h1>
-      <a href="/">Home</a>
-      <a href="/create" style={{
-          color:"white",
-          backgroundColor:"#f1356d",
-          borderRadius:'8px'
-      }}>New Blog</a>
+      <Link to="/">Home</Link>
+      <Link to="/create">New Blog</Link>
     </nav>
   );
 };
